6cf23ce330275b2b127f08de64b82a7ee2578eec
Logann Leriche <lleriche@c1r7p8.42lehavre.fr>
2026-07-21 16:25:53 +0200

    parsing

--- src/parsing.py
+++ src/parsing.py
@@ -10,11 +10,16 @@ class parser(BaseModel):
     def load_file(self):
         try:
             with open(self.path, 'r', encoding='utf-8') as f:
-                data = json.load(f)
-            print(data)
+                data = dict(json.load(f))
+            # verif que on a bien une liste d info
+            if not isinstance(data, dict):
+                print("error is not dict")
+                return []
+            return data
         except Exception as e:
             print(e)
+            return []
 
 
 c = parser(path="dataset_code_public.json")
-c.load_file()
+print(c.load_file())

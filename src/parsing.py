d869485e42ab9f4ae92bb1333045c55bd6053145
Logann Leriche <lleriche@c1r7p8.42lehavre.fr>
2026-07-21 12:23:26 +0200

    parsing

--- src/parsing.py
+++ src/parsing.py
@@ -4,3 +4,7 @@ import json
 
 class parser(BaseModel):
     path = Field(..., description="Path to the functions definition JSON file")
+
+    def load_file(self):
+        data = list(json.load(self.path))
+        print(data)

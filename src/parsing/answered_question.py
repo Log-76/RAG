6a69f65eda2c649de67ba863b657d919a38eb92f
Logann Leriche <lleriche@c1r7p8.42lehavre.fr>
2026-07-23 11:29:44 +0200

    dossier parsing

--- src/parsing/answered_question.py
+++ src/parsing/answered_question.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, Field
 from typing import List
-from minimal_source import MinimalSource
+from parsing.minimal_source import MinimalSource
 
 
 class UnansweredQuestion(BaseModel):

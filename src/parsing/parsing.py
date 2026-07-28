96749cfd34a611cdf88f67d66afc2a68e4064c6e
Logann Leriche <lleriche@c1r7p9.42lehavre.fr>
2026-07-28 10:43:24 +0200

    warning add recup extention file

--- src/parsing/parsing.py
+++ src/parsing/parsing.py
@@ -44,19 +44,6 @@ class parser(BaseModel):
             max_chunk = 2000
         return max_chunk
 
-    # def load_file(self):
-    # try:
-    #     with open(self.path, 'r', encoding='utf-8') as f:
-    #         data = dict(json.load(f))
-    #     # verif que on a bien une liste d info
-    #     if not isinstance(data, dict):
-    #         print("error is not dict", data)
-    #         return []
-    #     return data
-    # except Exception as e:
-    #     print(e)
-    #     return []
-
 
 c = parser(path="dataset_code_public.json")
 print(c.load_file())

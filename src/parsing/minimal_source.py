ae000bad8e4e762fdcedbc673083ebc766174f84
Logann Leriche <lleriche@c1r7p8.42lehavre.fr>
2026-07-29 15:38:55 +0200

    ajout de focntion data in minimalsource pour recupere les info stocke

--- src/parsing/minimal_source.py
+++ src/parsing/minimal_source.py
@@ -1,9 +1,12 @@
-from pydantic import BaseModel, Field
-from utils import error
+from pydantic import BaseModel
+
 
 class MinimalSource(BaseModel):
     file_path: str
     first_character_index: int
     last_character_index: int
 
-    
+    def data(self):
+        return {"file_path": self.file_path,
+                "first_character_index": self.first_character_index,
+                "last_character_index": self.last_character_index}

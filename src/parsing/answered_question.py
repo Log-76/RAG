1512397da64c8451ecd52e1aaedde40dd1d4d4d1
Logann Leriche <lleriche@c1r7p8.42lehavre.fr>
2026-07-30 12:55:31 +0200

    ajout import anszered_question.py import uuid

--- src/parsing/answered_question.py
+++ src/parsing/answered_question.py
@@ -1,3 +1,4 @@
+import uuid
 from pydantic import BaseModel, Field
 from typing import List
 from parsing.minimal_source import MinimalSource
@@ -5,8 +6,11 @@ from parsing.minimal_source import MinimalSource
 
 class UnansweredQuestion(BaseModel):
     question_id: str = Field(default_factory=lambda:
-    str(uuid.uuid4()))
+                             str(uuid.uuid4()))
     question: str
+
+    def UnansweredQuestion_data(self):
+        return {"question_id": self.question_id, "question": self.question}
 
 
 class AnsweredQuestion(UnansweredQuestion):

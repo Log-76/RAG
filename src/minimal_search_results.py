368bbeef52f9dfb8fe1e40c5c2f26e82ebb5d515
Logann Leriche <lleriche@c1r7p8.42lehavre.fr>
2026-07-30 17:50:26 +0200

    MinimalSearchResults MinimalAnswer

---
+++ src/minimal_search_results.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from typing import List
+from parsing.minimal_source import MinimalSource
+
+
+class MinimalSearchResults(BaseModel):
+    question_id: str
+    question: str
+    retrieved_sources: List[MinimalSource]
+
+
+class MinimalAnswer(MinimalSearchResults):
+    answer: str

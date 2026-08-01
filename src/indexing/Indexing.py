c7e311cd11a2a58569d21a47c27d44a3e84b1250
ele-roux <ewleroux@laposte.net>
2026-08-01 11:49:03 +0200

    Move: retrieve_data_from_minimal_source() in a separate file

--- src/indexing/Indexing.py
+++ src/indexing/Indexing.py
@@ -12,23 +12,6 @@ class Indexing():
         self.indexed_sources: list[MinimalSource] = []
         self.indexed_texts: list[str] = []
 
-    def retrieve_data_from_minimal_source(self, data: MinimalSource) -> str:
-        try:
-            with open(data.file_path, 'r', encoding='utf-8') as f:
-                raw_data = f.read()
-            if not raw_data:
-                error(f"raw_data does not exist for {data.file_path}")
-                return ""
-        except Exception as e:
-            error(f"error reading {data.file_path}: {e}")
-            return ""
-        try:
-            return raw_data[data.first_character_index:
-                             data.last_character_index]
-        except Exception as e:
-            error(f"error slicing {data.file_path}: {e}")
-            return ""
-
     def tokenize(self, text: str, file_type: str) -> list[str]:
         try:
             if file_type == "py":

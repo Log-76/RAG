8b4d9176f09dfc16e9f19933ecbc394119b837b8
Logann Leriche <lleriche@c1r7p10.42lehavre.fr>
2026-08-17 13:52:16 +0200

    retrieving

--- src/retrieving/Retrieving.py
+++ src/retrieving/Retrieving.py
@@ -1,13 +1,16 @@
 from langchain_community.retrievers import BM25Retriever
 from indexing import Indexing
 from pathlib import Path
+from parsing.parsing import parser
 
 
 class Retrieving():
-    def __init__(self, source: Path, k: int = 1):
+    def __init__(self, source: Path, questionpath: Path, k: int = 1):
         self.k: int = k
         self.source: Path = source
+        self.questionpath: Path = questionpath
         self.docs = Indexing.load(self.source)
+        self.question = parser.load_file(self.questionpath)
 
     def retrieving(self):
 
@@ -16,5 +19,5 @@ class Retrieving():
         retriever.k = self.k  # Nombre de résultats à renvoyer
 
         # Recherche
-        results = retriever.invoke("BM25 pertinence")
+        results = retriever.invoke(self.question)
         return results

0125d2ebb56ceee16ac985355f2dfac2d1f1fbe6
ele-roux <ewleroux@laposte.net>
2026-08-18 10:02:26 +0200

    Add: __main__ and CLI files

---
+++ src/__main__.py
@@ -0,0 +1,13 @@
+from .CLI import CLI
+import sys
+import fire
+
+def main() -> None:
+    try:
+        fire.Fire(CLI)
+    except Exception as e:
+        print(f"Erreur : {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
